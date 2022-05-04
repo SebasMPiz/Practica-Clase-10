795a4fe1a740eabe56451b835a0f770887059e4a
Sebastian Pizarro <le.sebiex@gmail.com>
2022-05-03 23:24:36 -0300

    Primer Commit

---
+++ app.js
@@ -0,0 +1,26 @@
+// // let express = require("express")
+// // let app = express()
+
+// // console.log(app)
+
+// const express = require('express');
+// const app = express();
+// app.listen(3000, () => console.log("Levantando un servidor con Express"))
+
+// const express = require('express');
+// const app = express();
+// // app.listen(3000, () => console.log("Levantando un servidor con Express"))
+// // app.get("/home", (req,res) => {
+// //     res.send("Hola, estamos en el home")
+// // })
+
+// const express = require('express');
+// const app = express();
+
+// let saludo = "Bienvenido/a, ahora estamos en tu perfil"
+
+// app.get("/perfil", (req, res) => {
+//     res.send(saludo)
+// })
+
+// let producto = {tipoProducto: null, precio:null, cantidad:null}
